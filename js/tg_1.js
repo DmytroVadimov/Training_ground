27768b8ce109fadc11aa23a4c29aac7298ccbfa8
Dmytro Vadimov <104729263+DmytroVadimov@users.noreply.github.com>
2024-03-16 15:49:37 +0200

    240316

--- js/tg_1.js
+++ js/tg_1.js
@@ -1,1 +1,14 @@
-"use strict";
+'use strict';
+const atTheOldToad = {
+    potions =[],
+    getPotions() {
+        return "List of all available potions";
+    },
+    addPotion(potionName) {
+        return `"Adding ${potionName}"`;
+    }
+};
+
+atTheOldToad.getPotions(); // "List of all available potions"
+atTheOldToad.addPotion('Invisibility'); // "Adding Invisibility"
+atTheOldToad.addPotion('Power potion'); // "Adding Power potion"
