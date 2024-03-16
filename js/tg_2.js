40aba6b47cbb158135c3cb2d72f42f0e9cd0f7e2
Dmytro Vadimov <104729263+DmytroVadimov@users.noreply.github.com>
2024-03-16 16:33:04 +0200

    240316_tg_2

--- js/tg_2.js
+++ js/tg_2.js
@@ -1,2 +1,18 @@
 'use strict';
-console.log('Hello Word');
+const atTheOldToad = {
+  potions: ['Speed potion', 'Stone skin'],
+  getPotions(propName) {
+    for (const potion of this.potions) {
+      if (potion === propName) {
+        return potion;
+      }
+    }
+    return 'List of all available potions';
+  },
+};
+
+console.log(atTheOldToad.getPotions('Speed potion'));
+console.log(atTheOldToad.getPotions('Doom'));
+console.log(atTheOldToad.getPotions());
+console.log(atTheOldToad.getPotions('Stone skin'));
+console.log(atTheOldToad.getPotions('stone skin'));
